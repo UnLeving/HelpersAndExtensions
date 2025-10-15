788d17b1f761686296d9fdd509d1fdfacbf8fa2e
ALeving <fgileving@gmail.com>
2025-10-15 15:52:08 +0300

    add new vector3 extension

--- Assets/Scripts/Helpers/Extensions/Vector3Extensions.cs
+++ Assets/Scripts/Helpers/Extensions/Vector3Extensions.cs
@@ -9,6 +9,11 @@ namespace Helpers.Extensions
             return new Vector3(x ?? original.x, y ?? original.y, z ?? original.z);
         }
 
+        public static Vector3 Add(this Vector3 original, float? x = null, float? y = null, float? z = null)
+        {
+            return new Vector3(original.x + (x ?? 0), original.y + (y ?? 0), original.z + (z ?? 0));
+        }
+
         public static bool IsBehind(this Vector3 queried, Vector3 forward)
         {
             return Vector3.Dot(queried, forward) < 0;
